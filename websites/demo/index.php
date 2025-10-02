63f1ea83381de77fe29eef6b46e4e1723a4a3cae
mouataz <mouatazbillah97@gmail.com>
2025-10-02 14:16:16 +0100

    setting up my MVC

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -1,4 +1,15 @@
 <?php
 
-require_once 'index.view.php';
+require_once 'functions.php';
 
+$uri = $_SERVER['REQUEST_URI'];
+
+if($uri == '/') {
+    require_once 'controllers/index.php';
+}else if($uri == '/about') {
+    require_once 'controllers/about.php';
+}else if($uri == '/contact') {
+    require_once 'controllers/contact.php';
+}else{
+    require_once 'controllers/404.php';
+}
